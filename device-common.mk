03c6a1fbf059566e6f412a7cf5cf0e90212318c9
Steven Moreland <smoreland@google.com>
2017-03-28 08:00:32 -0700

    Audio hal: toggled -> hwbinder
    Test: audio works, soundtrigger works, play music equalizer works
    Bug: 34274385
    Change-Id: I7fc1f9b83f242aced51f25923cee7cfe576c5c5f

--- device-common.mk
+++ device-common.mk
@@ -31,6 +31,7 @@ PRODUCT_SHIPPING_API_LEVEL := 16
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     audio.primary.msm8960 \
     audio.a2dp.default \
