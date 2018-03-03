303fc2bff7650d8cc13d03ff99eba0f185d982f0
Kevin Rocard <krocard@google.com>
2018-03-02 19:41:31 -0800

    Migrate to audio V4
    Test: regression test on the board
    Bug: 38184704
    Change-Id: Ife559125850277865451f92ea862e7ec47fc9da7
    Merged-In: Ife559125850277865451f92ea862e7ec47fc9da7
    Cherry-picked from master
    Signed-off-by: Kevin Rocard <krocard@google.com>

--- device-common.mk
+++ device-common.mk
@@ -30,9 +30,9 @@ PRODUCT_SHIPPING_API_LEVEL := 16
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
+    android.hardware.audio@4.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@4.0-impl \
     audio.primary.msm8960 \
     audio.a2dp.default \
     audio.usb.default \
